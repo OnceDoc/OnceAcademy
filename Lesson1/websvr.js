8e939a49fe8b7d67385940a91d573b4484e585ec
DXJ3X1 <zihua_xue@yahoo.com>
2016-10-14 11:12:41 +0800

    Add lesson1

---
+++ Lesson1/websvr.js
@@ -0,0 +1,15 @@
+//import WebSvr module
+var onceio = require("../onceio/onceio");
+
+//Start the WebSvr, running at current folder, default port is 8054, directory browser enabled;
+//Trying at: http://localhost:8054
+var app = onceio({
+    home: "./"
+  , listDir:  true
+  , debug:    true
+  , sessionTimeout: 60 * 1000
+});
+
+app.get("/", function(req, res) {
+  res.end('Hello, world')
+}) 
