828f95bc6e090d81906313dd1326fe61709d7398
DXJ3X1 <zihua_xue@yahoo.com>
2016-10-19 16:58:43 +0800

    Add Lesson3

---
+++ Lesson3/websvr.js
@@ -0,0 +1,32 @@
+var onceio = require('../onceio/onceio')
+
+var app = onceio({
+    home   :  "./"
+  , port   :  8054
+  , listDir:  true
+  , debug  :  false
+})
+
+app.get('/form', function(req, res) {
+  res.render('form.html')
+})
+
+//Handling form-data sent through the GET method
+app.get('/form/get_form.asp', function(req, res) {
+  res.write('Received the form-data:\n')
+  res.send('req.query: ' + JSON.stringify(req.query))
+})
+
+//Handling form-data sent through the POST method
+app.post('/form/post_form.asp', function(req, res) {
+  res.write('Received the form-data:\n')
+  res.send('req.body: ' + JSON.stringify(req.body))
+})
+
+//Handling form-data sent through the GET method and the POST method
+app.url('/form/get_and_post_form.asp/:routeParam', function(req, res) {
+  res.write('Received the form-data:\n')
+  res.write('req.params: ' + JSON.stringify(req.params) + '\n')
+  res.write('req.query: ' + JSON.stringify(req.query) + '\n')
+  res.send('req.body: ' + JSON.stringify(req.body))
+}, 'qs')
